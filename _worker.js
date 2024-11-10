cbb09004505bf648a0ce0a7f17b9d36d3d83361c
李力士 <lilishi@chinasofti.com>
2024-11-10 22:55:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,8 +10,8 @@ let lls = [
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	// 'icook.tw:2053#官方优选域名',
+	// 'cloudflare.cfgo.cc#优选官方线路',
 ].concat(lls);
 
 // 设置优选地址api接口
